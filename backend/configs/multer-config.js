b0356dffd6eec43857c04ff2e1351933ce03d44f
falguni-mondal <career.falguni@gmail.com>
2025-09-04 22:34:58 +0530

    Uninstalled jimp

--- backend/configs/multer-config.js
+++ backend/configs/multer-config.js
@@ -4,7 +4,7 @@ const crypto = require("crypto");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads"); // Location of uploading file
+    cb(null, "/tmp/uploads"); // Location of uploading file
   },
   filename: function (req, file, cb) {
     crypto.randomBytes(12, (err, name) => {
