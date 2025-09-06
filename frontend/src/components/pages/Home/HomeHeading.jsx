57641bd3636be9cd3243fb3ba413e9368087f6f6
falguni-mondal <career.falguni@gmail.com>
2025-09-06 20:13:22 +0530

    Fixed Docs page responsiveness

--- frontend/src/components/pages/Home/HomeHeading.jsx
+++ frontend/src/components/pages/Home/HomeHeading.jsx
@@ -6,7 +6,7 @@ const HomeHeading = () => {
         <div className="heading-container lg:col-span-2 text-[#dedede]">
             <h1 className='heading-txt'>Talk in Pixels Not in Words.</h1>
             <p className='heading-paragraph'>Securely encode your confidential messages within images, ensuring privacy, stealth, and easy retrieval with our advanced steganography technology—fast, reliable, and undetectable.</p>
-            <Link to="/docs" className='docs-btn block w-fit py-2.5 lg:py-2 px-8 lg:px-6 rounded-lg border-[#dedede] border font-bold whiteglow-hover transition-all duration-500'>Learn More</Link>
+            <Link to="/docs" className='learn-more-btn block w-fit py-2.5 lg:py-2 px-8 lg:px-6 rounded-lg border-[#dedede] border font-bold whiteglow-hover transition-all duration-500'>Learn More</Link>
         </div>
     )
 }
